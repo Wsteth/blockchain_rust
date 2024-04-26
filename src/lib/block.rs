7859414d2a177c0cb2a73a4e0cbcbc6d1ee4c848
Xingchen <wsteth@outlook.com>
2024-04-26 10:59:05 +0800

    feat: pow https://jeiwan.net/posts/building-blockchain-in-go-part-2/

--- src/lib/block.rs
+++ src/lib/block.rs
@@ -1,28 +1,33 @@
 use chrono::Local;
-use sha2::{Digest, Sha256};
+
+use crate::proof_of_work::ProofOfWork;
 
 pub struct Block {
     pub timestamp: i64,
     pub data: String,
     pub hash: String,
     pub prev_block_hash: String,
+    pub nonce: u64,
 }
 
 impl Block {
     pub fn new(data: &str, prev_block_hash: &str) -> Block {
         let timestamp = Local::now().timestamp();
-        let hash = generate_hash(prev_block_hash, timestamp, data);
+        let block = Block {
+            timestamp,
+            hash: String::new(),
+            data: data.to_string(),
+            prev_block_hash: prev_block_hash.to_string(),
+            nonce: 0,
+        };
+        let mut pow = ProofOfWork::new(block);
+        let (nonce, hash) = pow.run();
         Block {
             timestamp,
-            data: data.to_string(),
             hash,
+            data: data.to_string(),
             prev_block_hash: prev_block_hash.to_string(),
+            nonce,
         }
     }
-}
-
-fn generate_hash(prev_block_hash: &str, timestamp: i64, data: &str) -> String {
-    let mut hasher = Sha256::new();
-    hasher.update(format!("{}{}{}", prev_block_hash, timestamp, data));
-    format!("{:x}", hasher.finalize())
 }
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -2,3 +2,4 @@ pub mod block;
 pub mod blockchain;
 pub use block::Block;
 pub use blockchain::Blockchain;
+pub mod proof_of_work;
